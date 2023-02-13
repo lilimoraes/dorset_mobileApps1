bcd28d3d22afb0b28e824e3c618a67ccade0f177
lilimoraes <lilianmoraes@hotmail.com.br>
2023-02-13 14:30:13 +0000

    Activity Lifecycle

--- app/src/main/java/com/example/mobileapps1/MainActivity2.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity2.kt
@@ -5,7 +5,8 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity2 : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         Log.d("main_activity", "Activity created")
